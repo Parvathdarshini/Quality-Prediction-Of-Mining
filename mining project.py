db0dcf25f10a4bb37b67cfdbe5185ec4769bf117
Parvathdarshini <148962656+Parvathdarshini@users.noreply.github.com>
2024-06-30 16:09:53 +0530

    Quality prediction mining.py

--- mining project.py
+++ mining project.py
@@ -12,7 +12,6 @@ from matplotlib.pyplot import figure
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from pylab import *
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
